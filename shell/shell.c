45af70b66859337ce451d7e78a67b484cca4f49c
Tuotuoi <m18999624868_1@163.com>
2020-04-19 12:12:09 +0800

    cd&ls&pwd

--- shell/shell.c
+++ shell/shell.c
@@ -30,12 +30,52 @@ char *get_work_path() {
     //return path;
 }
 
-void do_exec_ls(char *path, char *cmd) {
-    printf(RED"do_exec_ls:"NONE "%s\n",path);
+char *get_cmd(char *cmd) {
+    char *new_cmd = (char *)malloc(sizeof(char) * 50);
+    int sub;
+    if(strlen(cmd) > 3) {
+        for(int i = 0; i < strlen(cmd); i++) {
+            if(cmd[i] == ' ') {
+                sub = i;
+                break;
+            }
+        }
+    } else return NULL;
+    strncpy(new_cmd,cmd + sub + 1,strlen(cmd) - sub);
+    *(new_cmd + strlen(cmd) - sub - 1) = '\0';
+    int len = strlen(new_cmd);
+    if(new_cmd[len - 1] == '\n') { //去除最后的回车
+        len --;
+        new_cmd[len] = 0;
+    } 
+    return new_cmd;
 }
 
-void do_exec_cd(char *path, char *cmd) {
-    printf(RED"do_exec_cd:\n"NONE);
+void do_exec_ls(char *path, char *cmd) {
+    printf(RED"do_exec_ls:"NONE "%s\n",path);
+    char *arg[] = {"ls",get_cmd(cmd),NULL};
+    pid_t pid;
+    if((pid = fork()) == 0) {
+        if(execvp("ls",arg) == -1) {
+            perror("execvp");
+            return ;
+        }
+    } else {
+        wait(NULL);
+    }
+}
+
+void do_exec_cd(char *cmd) {
+    char now_cmd[50];
+    memset(now_cmd,0,sizeof(now_cmd));
+    if(strcmp(cmd,"~") != 0)
+    strcpy(now_cmd ,get_cmd(cmd));
+    else strncpy(now_cmd,"~",1);
+    printf(RED"do_exec_cd: %s"NONE"\n",now_cmd);
+    if((chdir(now_cmd)) < 0) {
+        perror("chdir");
+        return;
+    }
 }
 
 void do_exec_pwd() {
@@ -53,7 +93,9 @@ int main() {
         if( strncmp(cmd,"ls",2) == 0) {
             do_exec_ls(path,cmd);
         } else if (strncmp(cmd,"cd",2) == 0) {
-            do_exec_cd(path,cmd);
+            if(strlen(cmd) <= 3)
+                do_exec_cd("~");
+            else do_exec_cd(cmd);
         } else if(strncmp(cmd,"pwd",3) == 0) {
             do_exec_pwd();
         } else if(strncmp(cmd,"\n",1) == 0) {
@@ -61,6 +103,7 @@ int main() {
         } else{
             printf("./bash:%s is not found!\n",cmd);
         }
+        path = get_work_path();
         printf("["GREEN"%s@localhost %s"NONE"]$ ",user_name,path);
         memset(cmd,0,strlen(cmd));
     }
